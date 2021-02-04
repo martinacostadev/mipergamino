2ff76447e3b82cb8dc4944928bdf2b25f25ea3ce
myke-roly <mike@underscope.io>
2021-02-04 02:16:41 -0300

    Configured prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: false,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
