3d6245988eefe0f46870d3b5ffed57cff090d17f
myke-roly <mike@underscope.io>
2021-02-13 23:39:39 -0300

    fixed linter

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
